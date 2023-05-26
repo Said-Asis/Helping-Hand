c5ce290b6195284216f41ed161fe5350bab3498e
EPUEQUE <epuequerocio@gmail.com>
2023-05-26 17:34:59 -0300

    Inicio del proyecto

---
+++ src/Componente/Boton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CameraButton from './CameraButton';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mi Aplicación</h1>
+      <CameraButton />
+    </div>
+  );
+};
+
+export default App;
