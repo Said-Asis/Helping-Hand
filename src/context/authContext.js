f6f16c317ee1f07774d28cee810502b61e485f2f
Said-Asis <saidasis42@gmail.com>
2023-11-13 23:36:06 -0300

    Creacion de autenticacion de usuario y rutas protejidas

---
+++ src/context/authContext.js
@@ -0,0 +1,47 @@
+import {createContext, useContext, useEffect, useState} from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, GithubAuthProvider } from "@firebase/auth";
+import {auth} from '../firebase';
+
+export const authContext = createContext();
+
+export const useAuth = () =>{
+    const context  = useContext(authContext)
+    if (!context) throw new Error('No auth provider :c')
+    return context
+}
+
+export function AuthProvider ({children}) {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const signup = (email, password)  => 
+        createUserWithEmailAndPassword(auth, email, password)
+
+    const login  = async (email, password)  => signInWithEmailAndPassword (auth, email, password);
+
+    const logout = () => signOut(auth)
+
+    const loginWithGoogle = ()  => {
+        const googleProvider = new GoogleAuthProvider()
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const loginWithGithub = ()  => {
+        const githubProvider = new GithubAuthProvider()
+        return signInWithPopup(auth, githubProvider)
+    }
+    
+    useEffect (() => {
+        onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+    }, [])
+
+    return (
+        <authContext.Provider value={{login, signup, user, logout, loading, loginWithGoogle, loginWithGithub}} >
+            {children}
+        </authContext.Provider>
+    )
+}
