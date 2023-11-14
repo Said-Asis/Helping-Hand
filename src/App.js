93376cc52090c2e26acc35b5d7f840d22eb3a99d
Said-Asis <saidasis42@gmail.com>
2023-11-13 23:31:51 -0300

    Agregado de rutas adicionales y privadas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import Login from './pages/Login';
 import Home from './pages/Home'
+import Register from './pages/Register'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {AuthProvider} from "./context/authContext"
+import { ProtectedRoute } from './pages/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes >
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route 
+              path='/'
+              element={ 
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>} 
+            />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
